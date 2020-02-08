487dd26905dcbd72837c498daacf824511a4ef2b
shapagat <shapagat2727@mail.ru>
2020-02-08 20:17:37 +0600

    first commit

--- Clima/Model/WeatherData.swift
+++ Clima/Model/WeatherData.swift
@@ -7,3 +7,16 @@
 //
 
 import Foundation
+
+struct WeatherData:Codable{
+    let name:String
+    let main: Main
+    let weather:[Weather]
+}
+struct Main:Codable {
+    let temp:Float
+}
+struct Weather:Codable{
+    let id:Int
+}
+
